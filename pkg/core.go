5abe656be94646d29db73cd2f99f27ca20538676
lhdht <15229684090@163.com>
2024-07-13 21:37:37 +0800

    feature: 发布消息（支持批量发布）

--- pkg/core.go
+++ pkg/core.go
@@ -65,3 +65,23 @@ func (core *CoreEntity) Lease(key string) clientv3.LeaseID {
 	}
 	return lease
 }
+
+func (core *CoreEntity) Pub(info *PubEntity) {
+	if cli, ce := core.Cli(info.CK); ce == nil {
+		lease := core.Lease(info.LK)
+		for _, raw := range info.Raw {
+			val, _ := json.Marshal(raw.Value)
+			if lease != 0 {
+				if _, err := cli.Put(context.Background(), raw.Key, string(val), clientv3.WithLease(lease)); err != nil {
+					core.logger.Error(err.Error())
+				}
+			} else {
+				if _, err := cli.Put(context.Background(), raw.Key, string(val)); err != nil {
+					core.logger.Error(err.Error())
+				}
+			}
+		}
+	} else {
+		core.logger.Error(ce.Error())
+	}
+}
