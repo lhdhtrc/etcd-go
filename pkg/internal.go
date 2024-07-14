ce1e9b4432c8a85d6b8985a261c2350e1ead07b6
lhdht <15229684090@163.com>
2024-07-14 13:30:03 +0800

    feature: 安装cli

---
+++ pkg/internal.go
@@ -0,0 +1,51 @@
+package etcd
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/client/pkg/v3/transport"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"strings"
+	"time"
+)
+
+func (core *CoreEntity) install(config *ConfigEntity) (*clientv3.Client, error) {
+	logPrefix := "setup etcd"
+	fmt.Printf("%s %s\n", logPrefix, "start ->")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	clientOptions := clientv3.Config{
+		DialTimeout: 5 * time.Second,
+		Endpoints:   strings.Split(config.Address, ","),
+		Context:     ctx,
+	}
+
+	if config.Account != "" && config.Password != "" {
+		clientOptions.Username = config.Account
+		clientOptions.Password = config.Password
+	}
+	if config.Tls.CaCert != "" && config.Tls.ClientCert != "" && config.Tls.ClientCertKey != "" {
+		tlsInfo := transport.TLSInfo{
+			CertFile:      config.Tls.ClientCert,
+			KeyFile:       config.Tls.ClientCertKey,
+			TrustedCAFile: config.Tls.CaCert,
+		}
+
+		tlsConfig, err := tlsInfo.ClientConfig()
+		if err != nil {
+			return nil, err
+		}
+		clientOptions.TLS = tlsConfig
+	}
+
+	cli, err := clientv3.New(clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("%s %s", logPrefix, "success ->")
+
+	return cli, nil
+}
