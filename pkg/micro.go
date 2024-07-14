1b8aa96eac03f44ebd4ade6f508e9cb053a1dbd5
lhdht <15229684090@163.com>
2024-07-14 13:32:18 +0800

    fix: 修改结构体

--- pkg/micro.go
+++ pkg/micro.go
@@ -1,4 +1,4 @@
-package pkg
+package etcd
 
 import (
 	"fmt"
@@ -10,11 +10,11 @@ func (core *CoreEntity) Register(service *ServiceEntity) {
 	ref := reflect.TypeOf(service.Srv).Elem()
 	length := ref.NumMethod()
 
-	lease := core.Lease(service.LK)
 	for i := 0; i < length; i++ {
-		core.Pub(lease, &RawEntity{
-			Key:   fmt.Sprintf("%s/%s/%s/%d", service.Namespace, service.Name, ref.Method(i).Name, lease),
+		core.Pub(&RawEntity{
+			Key:   fmt.Sprintf("%s/%s/%s/%d", service.Namespace, service.Name, ref.Method(i).Name, core.lease),
 			Value: service.Endpoint,
+			Lease: core.lease,
 		})
 	}
 }
