958dbb943df45b281e7e745b60b34a40f2e5b195
lhdht <15229684090@163.com>
2024-07-14 08:51:10 +0800

    perfect: 简化代码

--- pkg/pub.go
+++ pkg/pub.go
@@ -26,28 +26,9 @@ func (core *CoreEntity) Pub(lease clientv3.LeaseID, raw *RawEntity) {
 }
 
 // PubRaw batch send kv
-func (core *CoreEntity) PubRaw(info *PubEntity) {
-	if cli, ce := core.Cli(info.CK); ce == nil {
-		lease := core.Lease(info.LK)
-		for _, raw := range info.Raw {
-			var val string
-			if str, ok := raw.Value.(string); ok {
-				val = str
-			} else {
-				t, _ := json.Marshal(raw.Value)
-				val = string(t)
-			}
-			if lease != 0 {
-				if _, err := cli.Put(core.ctx, raw.Key, val, clientv3.WithLease(lease)); err != nil {
-					core.logger.Error(err.Error())
-				}
-			} else {
-				if _, err := cli.Put(core.ctx, raw.Key, val); err != nil {
-					core.logger.Error(err.Error())
-				}
-			}
-		}
-	} else {
-		core.logger.Error(ce.Error())
+func (core *CoreEntity) PubRaw(lk string, raw []*RawEntity) {
+	lease := core.Lease(lk)
+	for _, row := range raw {
+		core.Pub(lease, row)
 	}
 }
