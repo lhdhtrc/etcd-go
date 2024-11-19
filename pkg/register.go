9a5e1adb61f5abe30f387d11a3dc04da8dca5103
lhdht <15229684090@163.com>
2024-11-19 21:01:01 +0800

    fix: 暂存

--- pkg/register.go
+++ pkg/register.go
@@ -22,22 +22,22 @@ type ServiceInstance struct {
 // 网关接收到请求后 先去校验该服务节点是否存在 再去校验该方法是否存在
 
 // ---------------------------- mode-1 -----------------------------
-// device1 gateway NetworkId = 1 Ip = 192.168.1.100:1000
-// device1 service1 NetworkId = 1 Ip = 192.168.1.100:1001
-// device1 service1 NetworkId = 1 Ip = 192.168.1.100:1002
-// device1 service1 NetworkId = 1 Ip = 192.168.1.100:1003
+// device1 gateway NetworkId = 1 InternalNetIp = 192.168.1.100:1000 OuterNetIp = 117.34.9.1:1000
+// device1 service1 NetworkId = 1 InternalNetIp = 192.168.1.100:1001 OuterNetIp = 117.34.9.1:1001
+// device1 service1 NetworkId = 1 InternalNetIp = 192.168.1.100:1002 OuterNetIp = 117.34.9.1:1002
+// device1 service1 NetworkId = 1 InternalNetIp = 192.168.1.100:1003 OuterNetIp = 117.34.9.1:1003
 
 // ---------------------------- mode-1 -----------------------------
-// device1 gateway NetworkId = 1 Ip = 192.168.1.100:1000
-// device2 service1 NetworkId = 1 Ip = 192.168.1.101:1001
-// device3 service1 NetworkId = 1 Ip = 192.168.1.102:1002
-// device4 service1 NetworkId = 1 Ip = 192.168.1.103:1003
+// device1 gateway NetworkId = 1 InternalNetIp = 192.168.1.100:1000 OuterNetIp = 117.34.9.1:1000
+// device2 service1 NetworkId = 1 InternalNetIp = 192.168.1.101:1001 OuterNetIp = 117.34.9.2:1001
+// device3 service1 NetworkId = 1 InternalNetIp = 192.168.1.102:1002 OuterNetIp = 117.34.9.3:1002
+// device4 service1 NetworkId = 1 InternalNetIp = 192.168.1.103:1003 OuterNetIp = 117.34.9.4:1002
 
 // ---------------------------- mode-2 -----------------------------
-// server1 gateway NetworkId = 1 Ip = 192.168.1.100:1000
-// server2 service1 NetworkId = 2 Ip = 192.168.1.101:1001
-// server3 service1 NetworkId = 3 Ip = 192.168.1.102:1002
-// server4 service1 NetworkId = 4 Ip = 192.168.1.103:1003
+// server1 gateway NetworkId = 1 InternalNetIp = 192.168.1.100:1000 OuterNetIp = 117.34.9.1:1000
+// server2 service1 NetworkId = 2 InternalNetIp = 192.168.1.101:1001 OuterNetIp = 117.34.9.1:1001
+// server3 service1 NetworkId = 3 InternalNetIp = 192.168.1.102:1002 OuterNetIp = 117.34.9.1:1002
+// server4 service1 NetworkId = 4 InternalNetIp = 192.168.1.103:1003 OuterNetIp = 117.34.9.1:1003
 
 type Register struct {
 }
